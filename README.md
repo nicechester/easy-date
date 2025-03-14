8e376faff2392e80f9025a8f608b6fb7c513791e
Chester Kim <violkim@gmail.com>
2025-03-14 08:41:55 -0700

    [Issue-4] Support holidays

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Add following to pom.xml
 <dependency>
     <groupId>io.github.nicechester</groupId>
     <artifactId>easy-date</artifactId>
-    <version>0.2</version>
+    <version>0.3</version>
 </dependency>
 ```
 
@@ -39,43 +39,57 @@ Do you have tickets for March 10, June 21, 2025-12-25 for 2 adults and 1 kid?
 ### Find Date
 Parse text dates
 ```
-String[] ds = {"March tenth", "February second", "December twenty fifth", "Fourth July",
+String[] ds = {"New year day", "Martin Luther King Junior day", "Thanksgiving", "Christmas",
+        "March tenth", "February second", "December twenty fifth", "Fourth July",
         "today", "tomorrow", "yesterday", "next week", "next month"};
 for (String d : ds) {
-    System.out.println(FindDate.parse(d));
+    System.out.println(d + ": " + FindDate.parse(d));
 }
 ```
 results
 ```
-2025-03-10
-2025-02-02
-2025-12-25
-2025-07-04
-2025-03-12
-2025-03-13
-2025-03-11
-2025-03-19
-2025-04-12
+New year day: 2025-01-01
+Martin Luther King Junior day: null
+Thanksgiving: 2025-11-20
+Christmas: 2025-12-25
+March tenth: 2025-03-10
+February second: 2025-02-02
+December twenty fifth: 2025-12-25
+Fourth July: 2025-07-04
+today: 2025-03-14
+tomorrow: 2025-03-15
+yesterday: 2025-03-13
+next week: 2025-03-21
+next month: 2025-04-14
 ```
 
 Datize
 ```
-String source = "Do you have tickets for March tenth, June twenty first, first January, Fourth of July,"
-                + " 2025-12-25, today and tomorrow for two adults and 1 kid?";
+String source = "Do you have tix on tomorrow, april fifth, june twenty fifth, fourth of july and christmas with two adults and five kids?";
 System.out.println(FindDate.datize(source));
 ```
 results
 ```
-Do you have tickets for 2025-03-10, 2025-06-21, 2025-01-01, 2025-07-04, 2025-12-25, 2025-03-12 and 2025-03-13 for 2 adults and 1 kid?
+Do you have tix on 2025-03-15, 2025-04-05, 2025-06-25, 2025-07-04 and 2025-12-25 with 2 adults and 5 kids?
 ```
 
 Find dates
 ```
 String source = "Do you have tickets for March tenth, June twenty first, first January, Fourth of July,"
-        + " 2025-12-25, today and tomorrow for two adults and 1 kid?";
-System.out.println(FindDate.findDates(source));
+        + "Thanksgiving, Christmas day, 2025-12-31, today and tomorrow for two adults and 1 kid?";
+List<LocalDate> dates = FindDate.findDates(source);
+System.out.println(dates);
 ```
 results (`List<LocalDate>`)
 ```
-[2025-03-10, 2025-06-21, 2025-01-01, 2025-07-04, 2025-12-25, 2025-03-12, 2025-03-13]
+[2025-03-10, 2025-06-21, 2025-01-01, 2025-07-04, 2025-11-20, 2025-12-25, 2025-12-31, 2025-03-14, 2025-03-15]
+```
+
+Supported holidays
+```
+System.out.println(FindDate.supportedHolidays());
+```
+results
+```
+[juneteenth day, thanksgiving day, presidents day, juneteenth, columbus day, martin luther king junior day, christmas, veterans day, martin luther king day, memorial day, independence day, new year day, thanksgiving, christmas day, labor day]
 ```
--- src/main/java/org/easydate/FindDate.java
+++ src/main/java/org/easydate/FindDate.java
@@ -1,18 +1,29 @@
 package org.easydate;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Properties;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class FindDate {
     private static DateTimeFormatter TEXT_DATE_FORMAT1 = new DateTimeFormatterBuilder()
@@ -23,7 +34,66 @@ public class FindDate {
             .appendPattern("d MMMM")
             .parseDefaulting(ChronoField.YEAR, LocalDate.now().getYear())
             .toFormatter(Locale.ENGLISH);
+    private static DateTimeFormatter HOLIDAY_FORMAT = new DateTimeFormatterBuilder()
+            .appendPattern("MM-dd")
+            .parseDefaulting(ChronoField.YEAR, LocalDate.now().getYear())
+            .toFormatter(Locale.ENGLISH);
     private static final String MONTHS="(january|february|march|april|may|june|july|august|september|october|november|december)";
+    private static final HashMap<String, String> HOLIDAYS_ABSOLUTE;
+    private static final HashMap<String, RelativeDate> HOLIDAYS_RELATIVE;
+    private static final Collection<String> SUPPORTED_HOLIDAYS;
+    static {
+        SUPPORTED_HOLIDAYS = new HashSet<>();
+        HOLIDAYS_ABSOLUTE = new HashMap<>();
+        Properties prop = loadProperties("holidays-absolute.properties");
+        for (String key : prop.stringPropertyNames()) {
+            String holiday = key.toLowerCase().replace('_', ' ');
+            SUPPORTED_HOLIDAYS.add(holiday);
+            HOLIDAYS_ABSOLUTE.put(holiday, prop.getProperty(key));
+        }
+        HOLIDAYS_RELATIVE = new HashMap<>();
+        prop = loadProperties("holidays-relative.properties");
+        for (String key : prop.stringPropertyNames()) {
+            String holiday = key.toLowerCase().replace('_', ' ');
+            SUPPORTED_HOLIDAYS.add(holiday);
+            String[] values = prop.getProperty(key).split(",");
+            HOLIDAYS_RELATIVE.put(key, RelativeDate.of(values));
+        }
+    }
+
+    private static Properties loadProperties(String filename) {
+        Properties prop = new Properties();
+        try (InputStream input = FindDate.class.getClassLoader().getResourceAsStream(filename)) {
+            prop.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return prop;
+    }
+
+    private static class RelativeDate {
+        Month month;
+        DayOfWeek dayOfWeek;
+        int weekOffset;
+        static RelativeDate of(String[] values) {
+            RelativeDate relativeDate = new RelativeDate();
+            relativeDate.month = Month.valueOf(values[0]);
+            relativeDate.dayOfWeek = DayOfWeek.valueOf(values[1]);
+            relativeDate.weekOffset = Integer.valueOf(values[2]);
+            return relativeDate;
+        }
+    }
+
+    private static class CharPos {
+        int size;
+        String dateText;
+        static CharPos of(int size, String dateText) {
+            CharPos charPos = new CharPos();
+            charPos.size = size;
+            charPos.dateText = dateText;
+            return charPos;
+        }
+    }
 
     /**
      * parse text based date string.
@@ -31,7 +101,10 @@ public class FindDate {
      * @return
      */
     public static LocalDate parse(String dateString) {
-        String numerized = Numerizer.numerize(dateString);
+        Optional<LocalDate> holiday = parseHoliday(dateString);
+        if (holiday.isPresent()) {
+            return holiday.get();
+        }
         switch (dateString.toLowerCase()) {
             case "tomorrow":
                 return LocalDate.now().plusDays(1);
@@ -44,6 +117,7 @@ public class FindDate {
             case "next month":
                 return LocalDate.now().plusMonths(1);
         }
+        String numerized = Numerizer.numerize(dateString);
         DateTimeFormatter[] formatters = { TEXT_DATE_FORMAT1, TEXT_DATE_FORMAT2 };
         for (DateTimeFormatter dateFormat: formatters) {
             try {
@@ -56,15 +130,24 @@ public class FindDate {
         return null;
     }
 
-    private static class CharPos {
-        int size;
-        String dateText;
-        static CharPos of(int size, String dateText) {
-            CharPos charPos = new CharPos();
-            charPos.size = size;
-            charPos.dateText = dateText;
-            return charPos;
+    private static Optional<LocalDate> parseHoliday(String dateText) {
+        String text = dateText.toLowerCase();
+        if (HOLIDAYS_ABSOLUTE.containsKey(text)) {
+            String holiday = HOLIDAYS_ABSOLUTE.get(text);
+            return Optional.of(LocalDate.parse(holiday, HOLIDAY_FORMAT));
         }
+        if (HOLIDAYS_RELATIVE.containsKey(text)) {
+            RelativeDate relativeDate = HOLIDAYS_RELATIVE.get(text);
+            LocalDate date = LocalDate.of(LocalDate.now().getYear(), relativeDate.month, 1)
+                    .with(TemporalAdjusters.firstInMonth(relativeDate.dayOfWeek))
+                    .plusWeeks(relativeDate.weekOffset);
+            return Optional.of(date);
+        }
+        return Optional.empty();
+    }
+
+    public static Collection<String> supportedHolidays() {
+        return SUPPORTED_HOLIDAYS;
     }
 
     /**
@@ -90,7 +173,8 @@ public class FindDate {
             charPos.put(matcher.start(), CharPos.of(matcher.group().length(), dateText));
         }
 
-        pattern = Pattern.compile("(yesterday|today|tomorrow|next week|next month)");
+        pattern = Pattern.compile("(yesterday|today|tomorrow|next week|next month|"
+                + SUPPORTED_HOLIDAYS.stream().collect(Collectors.joining("|")) + ")");
         matcher = pattern.matcher(source.toLowerCase());
 
         while (matcher.find()) {
